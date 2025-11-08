a6ad8e76a171fbc3dc78a398a8d71cadae660e81
vvm-hash <vedvishnumarathe@gmail.com>
2025-11-09 04:33:42 +0530

    Disable lightningcss and turbopack for Vercel

--- campus-connect/next.config.js
+++ campus-connect/next.config.js
@@ -1,30 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack(config) {
-    // Disable LightningCSS by forcing Webpack CSS handling
-    config.module.rules.forEach(rule => {
-      if (Array.isArray(rule.use)) {
-        rule.use.forEach(loader => {
-          if (loader.loader?.includes("css-loader")) {
-            loader.options.modules = {
-              exportLocalsConvention: "as-is"
-            };
-          }
-        });
-      }
-    });
-
-    return config;
+  experimental: {
+    optimizeCss: false // disable lightningcss
   },
+  turbopack: false // force webpack instead of turbopack
+}
 
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "images.unsplash.com"
-      }
-    ]
-  }
-};
-
-module.exports = nextConfig;
+module.exports = nextConfig
